6e20adcbde929d44b0e78dae514e82dffa1f426d
Sabastian <sswbelser@gmail.com>
2015-08-06 09:45:49 -0700

    Format changes, added Navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,43 @@
 class UsersController < ApplicationController
-  before_filter :authorize, only: [:show]
+	before_filter :authorize, only: [:show]
 
-  def new
-    # redirect user if already logged in
-    if current_user
-      redirect_to profile_path
-    else
-      @user = User.new
-      render :new
-    end
-  end
+	def new
+		# redirect user if already logged in
+		if current_user
+			redirect_to profile_path
+		else
+			@user = User.new
+			render :new
+		end
+	end
 
-  def create
-    # redirect user if already logged in
-    if current_user
-      redirect_to profile_path
-    else
-      user = User.new(user_params)
-      if user.save
-        session[:user_id] = user.id
-        flash[:notice] = "Successfully signed up."
-        # redirect_to "/profile"
-        # refactored with route helpers:
-        redirect_to profile_path
-      else
-        flash[:error] = user.errors.full_messages.join(', ')
-        # redirect_to "/signup"
-        # refactored with route helpers:
-        redirect_to signup_path
-      end
-    end
-  end
+	def create
+		# redirect user if already logged in
+		if current_user
+			redirect_to profile_path
+		else
+			user = User.new(user_params)
+			if user.save
+				session[:user_id] = user.id
+				flash[:notice] = "Successfully signed up."
+				# redirect_to "/profile"
+				# refactored with route helpers:
+				redirect_to profile_path
+			else
+				flash[:error] = user.errors.full_messages.join(', ')
+				# redirect_to "/signup"
+				# refactored with route helpers:
+				redirect_to signup_path
+			end
+		end
+	end
 
-  def show
-    render :show
-  end
+	def show
+		render :show
+	end
 
-  private
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
-
+	private
+		def user_params
+			params.require(:user).permit(:email, :password, :password_confirmation)
+		end
 end
